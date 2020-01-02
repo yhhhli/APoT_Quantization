aa5e6b938fa89837dc5ad5d087aba0ad25d61617
codes4paper <loafyuhang@gmail.com>
2020-01-02 10:06:27 +0800

    update imagenet

---
+++ ImageNet/visualize.py
@@ -0,0 +1,174 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import torch
+import torch.nn as nn
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+from models.resnet import *
+from models.quant_layer import QuantConv2d, build_power_value, power_quantization, uniform_quantization
+
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (N, M).
+    row_labels
+        A list or array of length N with the labels for the rows.
+    col_labels
+        A list or array of length M with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    # cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    # cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    # ax.set_xticks(np.arange(data.shape[1]))
+    # ax.set_yticks(np.arange(data.shape[0]))
+    # # ... and label them with the respective list entries.
+    # ax.set_xticklabels(col_labels)
+    # ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    # ax.tick_params(top=True, bottom=False,
+    #                labeltop=True, labelbottom=False)
+
+    # # Rotate the tick labels and set their alignment.
+    # plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+    #          rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+
+    plt.xticks([], [])
+    plt.yticks([], [])
+
+    return im
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["black", "white"],
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A list or array of two color specifications.  The first is used for
+        values below a threshold, the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            display_text, fontsize = find_apot_level(data[i,j])
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)], fontsize=fontsize)
+            text = im.axes.text(j, i, display_text, **kw)
+            texts.append(text)
+
+    return texts
+
+
+def find_apot_level(data):
+    if data - 0.0 == 0.0:
+        return '0', 12
+    else:
+        sign = np.sign(data)
+        first_term = np.log2(np.abs(data))
+        if first_term == np.round(first_term):
+            return (r'$2^{%d}$'%first_term if sign==1 else r'$-2^{%d}$'%first_term), 11
+        else:
+            second_term = np.log2(np.abs(data)-2**np.floor(first_term))
+            return (r'$2^{%d}+2^{%d}$'%(np.floor(first_term), second_term) if sign==1 else r'$-(2^{%d}+2^{%d})$'%(np.floor(first_term),second_term)), 9
+
+
+
+model = resnet18()
+checkpoint = torch.load('checkpoint/res18_5bit.pth.tar', map_location='cpu')          # load checkpoint into cpu
+state_dict = {k.replace('module.', ''): v for k, v in checkpoint.items()}
+model.load_state_dict(state_dict)
+weights_q_matrix = None
+alpha = None
+for m in model.modules():                                                    # load weights in the first quantized layer
+    if isinstance(m, QuantConv2d):
+        weights_q_matrix = m.weight_quant(m.weight.data)
+        weights_q_matrix = weights_q_matrix.div(m.weight_quant.wgt_alpha.item())*1.5
+        alpha = m.weight_quant.wgt_alpha.item()
+        break
+weights_q_matrix = weights_q_matrix[0][0].reshape(3,3).detach().numpy() # select the first 81 elements in the first kernal
+
+fig, ax = plt.subplots(figsize=[3,3], dpi=100)
+im= heatmap(weights_q_matrix, 3, 3, ax=ax,
+                   cmap="YlGn", cbarlabel="harvest [t/year]")
+texts = annotate_heatmap(im, valfmt="{x:.1f} t")
+
+fig.tight_layout()
+plt.savefig('weight_visual.png')
+plt.show()
